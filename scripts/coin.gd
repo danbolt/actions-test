5056ec9d6088c9422c530ef01b53ffc5a98f97de
Daniel Savage <dansvg@gmail.com>
2025-01-23 15:42:38 -0800

    Coin colour

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,7 @@
 class_name Coin extends Area2D
 
+@onready var coin_polygon: Node2D = $Polygon2D
+
 func on_player_entered(intruder: Node2D) -> void:
 	assert(intruder is Player)
 	(intruder as Player).collect_coin()
@@ -7,3 +9,6 @@ func on_player_entered(intruder: Node2D) -> void:
 
 func _ready() -> void:
 	body_entered.connect(on_player_entered)
+
+func _process(delta: float) -> void:
+	coin_polygon.rotate(delta * 10)
